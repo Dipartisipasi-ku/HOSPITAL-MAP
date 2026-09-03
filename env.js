7823cd0528ef02014683e93e6315657ec0d7287c
Dipartisipasi-ku <dipartisipasi.ku@gmail.com>
2026-09-04 07:44:41 +0800

    Add files via upload

---
+++ env.js
@@ -0,0 +1,7 @@
+// File: env.js
+// PENTING: Jika menggunakan Git/GitHub, masukkan file "env.js" ini ke dalam .gitignore
+
+export const ENV = {
+    SUPABASE_URL: "https://rbliuccimrcopxjgidfk.supabase.co",
+    SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJibGl1Y2NpbXJjb3B4amdpZGZrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODgzODcwMDYsImV4cCI6MjEwMzk2MzAwNn0.lckukXr0rQsIKKhZ6cRLu2NG-9JHsMFs7xVraeJPFmI"
+};
